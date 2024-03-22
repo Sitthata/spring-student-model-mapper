3bf58be431ffb75166d4c1ccefc4e4f3f8c868eb
Sitthata <aprils96950@gmail.com>
2024-03-22 12:10:25 +0700

    Finished file service

--- src/main/java/sit/example/int204springapi/controller/FileController.java
+++ src/main/java/sit/example/int204springapi/controller/FileController.java
@@ -1,19 +1,38 @@
 package sit.example.int204springapi.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import sit.example.int204springapi.properties.FileStorageProperties;
+import sit.example.int204springapi.service.FileService;
 
 @RestController
 @RequestMapping("/api/file")
 @RequiredArgsConstructor
 public class FileController {
     private final FileStorageProperties fileStorageProperties;
+    private final FileService fileService;
+
+    @PostMapping("")
+    public ResponseEntity<Object> upload(@RequestParam("file") MultipartFile file) {
+        fileService.store(file);
+        return ResponseEntity.ok(String.format("File %s uploaded successfully", file.getOriginalFilename()));
+    }
+
+    @GetMapping("/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        Resource file = fileService.loadFileAsResource(filename);
+        String extension = filename.substring(filename.lastIndexOf('.'));
+        MediaType extensionType = extension.equals("png") ? MediaType.IMAGE_PNG : MediaType.IMAGE_JPEG;
+        return ResponseEntity.ok().contentType(extensionType).body(file);
+    }
+
     @GetMapping("test")
-    public ResponseEntity<String> test() {
-        return ResponseEntity.ok(fileStorageProperties.getUploadDir());
+    public ResponseEntity<Object> test() {
+        return ResponseEntity.ok(fileStorageProperties);
     }
 }
--- src/main/java/sit/example/int204springapi/service/FileService.java
+++ src/main/java/sit/example/int204springapi/service/FileService.java
@@ -1,15 +1,21 @@
 package sit.example.int204springapi.service;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 import sit.example.int204springapi.properties.FileStorageProperties;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 
 @Service
 @Getter
@@ -28,6 +34,46 @@ public class FileService {
             }
         } catch (IOException ex) {
             throw new RuntimeException("Could not create the directory where the uploaded files will be stored.", ex);
+        }
+    }
+
+    public String store(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+//        String fileTest;
+         try {
+             if (fileName.contains("..")) {
+                 throw new RuntimeException("Sorry! Filename contains invalid path sequence " + fileName);
+             }
+             Path targetLocation = this.fileStorageLocation.resolve(fileName);
+             Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+             return fileName;
+         } catch (IOException ex) {
+             throw new RuntimeException("Could not store file " + fileName + ". Please try again!", ex);
+         }
+    }
+
+    public Resource loadFileAsResource(String fileName) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (!resource.exists()) {
+                throw new RuntimeException("File not found");
+            }
+            return resource;
+        } catch (MalformedURLException ex) {
+            throw new RuntimeException("File operation error", ex);
+        }
+    }
+
+    public void removeResource(String filename) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(filename).normalize();
+            if (!Files.exists(filePath)) {
+                throw new RuntimeException("File not found " + filename);
+            }
+            Files.delete(filePath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
